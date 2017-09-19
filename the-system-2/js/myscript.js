a2fe2df0503bdbb24fce0368a932a47b9c17ec42
idsnews <idsnews@users.noreply.github.com>
2017-09-18 21:55:23 -0400

    sexual assault project day 2
    day 2

---
+++ the-system-2/js/myscript.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+var $window = $(window); //You forgot this line in the above example
+
+$('section[data-type="background"]').each(function(){
+var $bgobj = $(this); // assigning the object
+$(window).scroll(function() {
+var yPos = -( ($window.scrollTop() + 400) / $bgobj.data('speed'));
+// Put together our final background position
+var coords = '50% '+ yPos + 'px';
+// Move the background
+// $bgobj.css({ backgroundPosition: coords });
+});
+});
+});
+
+// Create HTML5 elements for IE
+          
+
+                    document.createElement("section");
+                    $(function() {
+  $("img.scale").imageScale();
+});
