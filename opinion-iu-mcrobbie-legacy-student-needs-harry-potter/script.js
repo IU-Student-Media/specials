37e886967f547e15a584cee4ac966430b7b80d2b
idsnews <idsnews@users.noreply.github.com>
2020-09-14 16:04:34 -0400

    number 3
    better links and additional information

--- opinion-iu-mcrobbie-legacy-student-needs-harry-potter/script.js
+++ opinion-iu-mcrobbie-legacy-student-needs-harry-potter/script.js
@@ -41,32 +41,32 @@
             //get info for head
             var h = document.getElementById('head');
             h.style.position = 'absolute';
-            h.style.left = pos.x + 'px';
+            h.style.left = (pos.x+10) + 'px';
             h.style.top = pos.y + 'px';
 
             //get info for scroll
             var s = document.getElementById('scroll');
             s.style.position = 'absolute';
-            s.style.left = (pos.x+5) + 'px';
+            s.style.left = (pos.x+15) + 'px';
             s.style.top = (pos.y-25) + 'px';
         }
     }
 })();
 
-function footsteps() {
-document.addEventListener('touchstart', function(e) {
-  // Iterate through the touch points and log each screenX/Y coordinate.
-  // The unit of each coordinate is CSS pixels.
-  var i;
-  for (i=0; i < e.touches.length; i++) {
-    var f = document.getElementById('footsteps');
-    f.style.position = 'absolute';
-    f.style.left = (e.touches[i].screenX-323) + 'px';
-    f.style.top = (e.touches[i].screenY-175) + 'px';
-    console.log((e.touches[i].screenX-323) + ', ' + (e.touches[i].screenY-175));
-
-    // console.log("touchpoint[" + i + "].screenX = " + e.touches[i].screenX);
-    // console.log("touchpoint[" + i + "].screenY = " + e.touches[i].screenY);
-  }
-}, false);
-};
+// function footsteps() {
+// document.addEventListener('touchstart', function(e) {
+//   // Iterate through the touch points and log each screenX/Y coordinate.
+//   // The unit of each coordinate is CSS pixels.
+//   var i;
+//   for (i=0; i < e.touches.length; i++) {
+//     var f = document.getElementById('footsteps');
+//     f.style.position = 'absolute';
+//     f.style.left = (e.touches[i].screenX-323) + 'px';
+//     f.style.top = (e.touches[i].screenY-175) + 'px';
+//     console.log((e.touches[i].screenX-323) + ', ' + (e.touches[i].screenY-175));
+//
+//     // console.log("touchpoint[" + i + "].screenX = " + e.touches[i].screenX);
+//     // console.log("touchpoint[" + i + "].screenY = " + e.touches[i].screenY);
+//   }
+// }, false);
+// };
