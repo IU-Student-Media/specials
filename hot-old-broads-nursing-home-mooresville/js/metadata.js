28efacad7ea99043efd6f1f2a42e74c7571133d8
Carson TerBush <61434744+carsonology@users.noreply.github.com>
2022-11-29 12:22:09 -0500

    marissa profile

---
+++ hot-old-broads-nursing-home-mooresville/js/metadata.js
@@ -0,0 +1,105 @@
+const data = {
+    "url": "hot-old-broads-nursing-home-mooresville",
+    "slug": "Darla and the Hot Old Broads",
+    "headline": "'Leave no broad behind': Darla refuses to let people be forgotten",
+    "title": "Nursing Home",
+    "pub_date": "Nov. 29, 2022",
+    "bylines": {
+        "Story, design and development by": {
+            "name": "Marissa Meador",
+            "email": "marnmead@iu.edu",
+            "twitter": "marissa_meador",
+            "pfp": "https://s3.amazonaws.com/snwceomedia/ids/5b797f8c-c389-4f3a-b42f-6ec84ae09551.sized-1000x1000.jpg",
+            "bio": "Marissa has worked at the IDS since 2022 as a reporter and editor."
+        },
+        "Photos by": [{
+
+            "name": "Cora Shaw",
+            "email": "shawcora@iu.edu",
+            "twitter": "corashaww",
+            "pfp": "https://s3.amazonaws.com/snwceomedia/ids/8c6b67b9-1b85-4ecb-b037-a8283a7652de.sized-1000x1000.jpg",
+            "bio": "Cora is a freshman studying media advertising. She works for the IDS as a photographer."
+        },
+        {
+            "name": "Marissa Meador",
+        }]
+
+    }
+}
+
+// bylines
+const byline_types = ["By", "Story, design and development by", "Photos by", "Graphics by"]
+let bylines_html = '';
+let bios_html = '';
+
+for (let type of byline_types) {
+    if (data.bylines[type]) {
+        if (data.bylines[type].length) {
+            data.bylines[type].forEach((author) => setAuthorBio(type, author));
+        } else {
+            setAuthorBio(type, data.bylines[type]);
+        }
+    }
+}
+
+for (let type of byline_types) {
+    if (data.bylines[type]) {
+
+        if (!data.bylines[type].length) {
+            bylines_html += `<p>${type} <a target="_blank" href="https://idsnews.com/staff/${data.bylines[type].name.split(' ').join('-')}">${data.bylines[type].name}</a></p>`;
+        } else {
+            bylines_html += '<p>' + type;
+            for (let index in data.bylines[type]) {
+                bylines_html += ` <a target="_blank" href="https://idsnews.com/staff/${data.bylines[type][index].name.split(' ').join('-')}">${data.bylines[type][index].name}</a>`;
+                if (index != data.bylines[type].length - 2 && index != data.bylines[type].length - 1) {
+                    bylines_html += ',';
+                } else if (index == data.bylines[type].length - 2) {
+                    bylines_html += ' and';
+                }
+            }
+            bylines_html += '</p>';
+        }
+    }
+
+}
+
+function setAuthorBio(type, author) {
+    let twitter_link = `<span><a href="https://twitter.com/${author.twitter}" target="_blank">Twitter <i class="fab fa-twitter"></i></a></span>`;
+    let email_link = `<span style="padding-right: var(--xs); padding-left: var(--xxs);"><a
+        href="mailto:${author.email}" target="_blank">Email <i class="fa fa-envelope"></i></a></span>`;
+    if (author.pfp && author.bio) {
+        bios_html +=
+            `<div class="bio">
+                        <div>
+                            <img src="${author.pfp}" alt="${author.name}">
+                            <div>
+                            <p>${type} <a href="https://idsnews.com/staff/${author.name.split(' ').join('-')}" target="_blank">${author.name}</a></p>
+                            <p>${author.bio}  ${author.email ? email_link : ''}   ${author.twitter ? twitter_link : ''}</p>
+                            </div>
+                        </div>
+                    </div>`;
+    }
+}
+
+document.querySelector('#bylines').innerHTML = bylines_html;
+document.querySelector('.author-bios').innerHTML = bios_html;
+
+// pubdate
+document.querySelector('#pubdate').innerHTML = "Published " + data.pub_date;
+
+// title & slug
+document.querySelector('title').innerHTML = data.title + ' | Indiana Daily Student';
+document.querySelector('#slug').innerHTML = data.slug;
+
+// socials
+let meta_twitter = document.querySelectorAll('meta[name*="twitter"]');
+let meta_og = document.querySelectorAll('meta[property*="og"]');
+let fb = `https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fspecials.idsnews.com%2F${data.url}`;
+let twitter = `https://twitter.com/intent/tweet?url=https%3A%2F%2Fspecials.idsnews.com%2F${data.url}%2F&text=${data.headline.split(' ').join('%20')}`;
+let reddit = `https://www.reddit.com/submit?title=${data.headline.split(' ').join('%20')}&url=http%3A%2F%2Fspecials.idsnews.com%2F${data.url}`;
+
+document.querySelector('li#socials').innerHTML = `
+        <a href="${fb}" target="_blank"><i class="fab fa-facebook"></i></a>
+        <a href="${twitter}" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="${reddit}" target="_blank"><i class="fab fa-reddit"></i></a>
+        `;
