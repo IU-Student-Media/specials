03de41a5208ecc2124c2f43afddf61c27627fc22
idsnews <idsnews@users.noreply.github.com>
2022-11-30 13:15:49 -0500

    files from Ethan
    Ad code corrected.

---
+++ indiana-north-carolina-basketball-blue-bloods/README.md
@@ -0,0 +1,2 @@
+<h1>IU men's basketball v. UNC preview!</h1>
+
---
+++ indiana-north-carolina-basketball-blue-bloods/js/metadata.js
@@ -0,0 +1,100 @@
+const data = {
+    "url": "indiana-north-carolina-basketball-blue-bloods",
+    "slug": "IUMBB vs. UNC preview",
+    "headline": "Battle of the Blue Bloods: The recent history of Indiana and North Carolina men's basketball",
+    "title": "IU men's basketball vs. UNC preview",
+    "pub_date": "Nov. 30, 2022",
+    "bylines": {
+        "By": {
+            "name": "Emma Pawlitz",
+            "email": "epawlitz@iu.edu",
+            "twitter": "emmapawlitz",
+            "pfp": "https://s3.amazonaws.com/snwceomedia/ids/9f8b83d0-e57c-4655-b07a-e2fb7f4de9ba.original.jpg",
+            "bio": "Emma is a sports editor and an IU men's basketball reporter. She has worked for the IDS since 2021."
+        },
+        "Design and development by": {
+            "name": "Ethan Moore",
+            "email": "ethmoore@iu.edu",
+            "twitter": "ethcmoore",
+            "pfp": "https://s3.amazonaws.com/snwceomedia/ids/2aefdae2-d850-4d64-a035-690b1f706337.original.jpg",
+            "bio": "Ethan has worked at the IDS since 2020 as a photojournalist, news reporter, print designer and web designer."
+        },
+    }
+}
+
+// bylines
+const byline_types = ["By", "Photos by", "Design and development by", "Graphics by"]
+let bylines_html = '';
+let bios_html = '';
+
+for (let type of byline_types) {
+    if (data.bylines[type]) {
+        if (data.bylines[type].length) {
+            data.bylines[type].forEach((author) => setAuthorBio(type, author));
+        } else {
+            setAuthorBio(type, data.bylines[type]);
+        }
+    }
+}
+
+for (let type of byline_types) {
+    if (data.bylines[type]) {
+
+        if (!data.bylines[type].length) {
+            bylines_html += `<p>${type} <a href="https://idsnews.com/staff/${data.bylines[type].name.split(' ').join('-')}">${data.bylines[type].name}</a></p>`;
+        } else {
+            bylines_html += '<p>' + type;
+            for (let index in data.bylines[type]) {
+                bylines_html += ` <a href="https://idsnews.com/staff/${data.bylines[type][index].name.split(' ').join('-')}">${data.bylines[type][index].name}</a>`;
+                if (index != data.bylines[type].length - 2 && index != data.bylines[type].length - 1) {
+                    bylines_html += ',';
+                } else if (index == data.bylines[type].length - 2) {
+                    bylines_html += ' and';
+                }
+            }
+            bylines_html += '</p>';
+        }
+    }
+
+}
+
+function setAuthorBio(type, author) {
+    let twitter_link = `<span><a href="https://twitter.com/${author.twitter}" target="_blank">Twitter <i class="fab fa-twitter"></i></a></span>`;
+    let email_link = `<span style="padding-right: var(--xs); padding-left: var(--xxs);"><a
+        href="mailto:${author.email}" target="_blank">Email <i class="fa fa-envelope"></i></a></span>`;
+    if (author.pfp && author.bio) {
+        bios_html +=
+            `<div class="bio">
+                        <div>
+                            <img src="${author.pfp}" alt="${author.name}">
+                            <div>
+                            <p>${type} <a href="https://idsnews.com/staff/${author.name.split(' ').join('-')}" target="_blank">${author.name}</a></p>
+                            <p>${author.bio}  ${author.email ? email_link : ''}   ${author.twitter ? twitter_link : ''}</p>
+                            </div>
+                        </div>
+                    </div>`;
+    }
+}
+
+document.querySelector('#bylines').innerHTML = bylines_html;
+document.querySelector('.author-bios').innerHTML = bios_html;
+
+// pubdate
+document.querySelector('#pubdate').innerHTML = "Published " + data.pub_date;
+
+// title & slug
+document.querySelector('title').innerHTML = data.title + ' | Indiana Daily Student';
+document.querySelector('#slug').innerHTML = data.slug;
+
+// socials
+let meta_twitter = document.querySelectorAll('meta[name*="twitter"]');
+let meta_og = document.querySelectorAll('meta[property*="og"]');
+let fb = `https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fspecials.idsnews.com%2F${data.url}`;
+let twitter = `https://twitter.com/intent/tweet?url=https%3A%2F%2Fspecials.idsnews.com%2F${data.url}%2F&text=${data.headline.split(' ').join('%20')}`;
+let reddit = `https://www.reddit.com/submit?title=${data.headline.split(' ').join('%20')}&url=http%3A%2F%2Fspecials.idsnews.com%2F${data.url}`;
+
+document.querySelector('li#socials').innerHTML = `
+        <a href="${fb}" target="_blank"><i class="fab fa-facebook"></i></a>
+        <a href="${twitter}" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="${reddit}" target="_blank"><i class="fab fa-reddit"></i></a>
+        `;
