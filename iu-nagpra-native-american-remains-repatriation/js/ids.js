cd2aa598b56864a3d19267bdc991fb1dd1599796
Carson TerBush <61434744+carsonology@users.noreply.github.com>
2023-04-28 01:18:08 -0400

    clean up file

--- iu-nagpra-native-american-remains-repatriation/js/ids.js
+++ iu-nagpra-native-american-remains-repatriation/js/ids.js
@@ -7,19 +7,6 @@
 const nav = document.querySelector('nav');
 document.querySelector('.logo span').style.display = 'block';
 const header = document.querySelector('header');
-// function checkScroll() { // callback function 
-//     let startY = header.offsetHeight * 0.6; //The point where the navbar changes in px
-//     // check whether scroll position is above/below target
-//     // toggle css styles accordingly
-//     if(window.scrollY > startY) { 
-//         nav.style.backgroundColor = 'black';
-//         slug.style.display = 'block';
-//     } else {
-//         nav.style.backgroundColor = 'transparent';
-//         slug.style.display = 'none';
-//     }
-// }
-// window.addEventListener('scroll', checkScroll);
 
 /*
     DROPDOWN MENU
@@ -63,41 +50,3 @@ moveItItem.prototype.update = function (scrollTop) {
 $(function () {
     $('[data-scroll-speed]').moveIt();
 });
-
-
-// html table
-const tableDiv = document.querySelector('#inventory-table');
-// tableDiv.innerHTML = '<'
-
-// let tableData = []
-let tableHTML = '';
-d3.csv('files/inventory-reports.csv', (row, i) => {
-    // let tableHTML;
-    if (i === 0) {
-        tableHTML += '<thead><tr>'
-        // console.log('first object')
-        Object.keys(row).forEach((item, j) => {
-            // console.log(item)
-            tableHTML += `<th ${j == 0 ? 'style="width:13%;text-align:left;"' : ''} ${j == 3 | j == 4 ? 'style="text-align:right;"' : 'style="text-align:left;"'}>${item}</th>`
-        })
-        tableHTML += '</thead></tr><tbody>'
-    }
-
-    Object.values(row).forEach((item, j) => {
-        // console.log(item)
-        tableHTML += `<td ${j == 3 | j == 4 ? 'style="text-align:right;"' : ''}>${item}</td>`
-    })
-    tableHTML += '</tr>'
-
-    // tableDiv.insertAdjacentHTML('beforeend', tableHTML)
-    // console.log(tableData)
-});
-
-setTimeout(afterLoad, 300);
-
-function afterLoad() {
-    console.log('timeout')
-    console.log(tableHTML)
-    tableDiv.insertAdjacentHTML('beforeend', tableHTML)
-}
-
