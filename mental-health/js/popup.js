ed7e4013295265ccdde80810b98032bb2ec1f30a
carsonology <carson.terbush@gmail.com>
2023-05-02 01:09:36 -0400

    Updates

--- mental-health/js/popup.js
+++
@@ -1,4 +0,0 @@
-$('.popup').click(function(event) {
-    event.preventDefault();
-    window.open($(this).attr("href"), "popupWindow", "width=600,height=400,scrollbars=yes");
-});
--- yaolin/js/script.js
+++
@@ -1,3 +0,0 @@
-// $('.carousel').carousel({
-//     interval: false
-// }) 
