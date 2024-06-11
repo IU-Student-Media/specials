8dda174d26f33f1b397933e7ae170b6c998054dd
idsnews <idsnews@users.noreply.github.com>
2024-06-11 15:38:05 -0400

    photo gallery
    Garnfalloon upload

---
+++ bands-take-main-stage-for-2024-granfalloon-finale/README.md
@@ -0,0 +1,12 @@
+<h1>&#x1F3B8;About this project</h1>
+<p>Photo buildout for Granfalloon 2024</p>
+
+<h1>:computer: Template Info</h1>
+
+
+<h2>Instructions:</h2>
+<ul>
+  <li>Check the <a href="https://www.notion.so/Buildout-Documentation-4e36a30949d149e5bb074a753a88a910">Documentation</a> for the source code of our default story elements</li>
+  <li>Only change styles.css, don't mess with ids.css or normalize.css</li>
+</ul>
+<em>More specific instructions listed on <a href="https://www.notion.so/invite/17d2acf01007e49087f6ed4138c3f874fd9d3cca">our documentation</a>!.</em>
---
+++ bands-take-main-stage-for-2024-granfalloon-finale/js/metadata.js
@@ -0,0 +1,101 @@
+const data = {
+    "url": "bands-take-main-stage-for-2024-granfalloon-finale",
+    // slug will appear in the nav bar
+    "slug": "Bands take main stage for 2024 Granfalloon finale",
+    // title will appear on the tab in the browser
+    "title": "Bands take main stage for 2024 Granfalloon finale",
+    "headline": "Bands take main stage for 2024 Granfalloon finale",
+    // make sure to update the pub date before finishing the project
+    "pub_date": "June 12, 2024",
+    "bylines": {
+        // fill in photographer info
+        "Photos by": [
+            {
+                "name": "Zuzanna Kukawska",
+                "email": "zkukawsk@iu.edu",
+            }
+        ],
+        // fill in digital designer info
+        "Design and development by": {
+            "name": "Reece Needham",
+            "email": "reeneedh@iu.edu",
+        }
+    }
+}
+
+// bylines
+const byline_types = ["By", "Photos by", "Design and development by", "Graphics by"]
+let bylines_html = '';
+let bios_html = '';
+
+for (let type of byline_types) {
+    if (data.bylines[type]) {
+        if (data.bylines[type].length) {
+            data.bylines[type].forEach((author) => setAuthorBio(type, author));
+        } else {
+            setAuthorBio(type, data.bylines[type]);
+        }
+    }
+}
+
+for (let type of byline_types) {
+    if (data.bylines[type]) {
+
+        if (!data.bylines[type].length) {
+            bylines_html += `<p>${type} <a href="https://idsnews.com/staff/${data.bylines[type].name.split(' ').join('-')}">${data.bylines[type].name}</a></p>`;
+        } else {
+            bylines_html += '<p>' + type;
+            for (let index in data.bylines[type]) {
+                bylines_html += ` <a href="https://idsnews.com/staff/${data.bylines[type][index].name.split(' ').join('-')}">${data.bylines[type][index].name}</a>`;
+                if (index != data.bylines[type].length - 2 && index != data.bylines[type].length - 1) {
+                    bylines_html += ',';
+                } else if (index == data.bylines[type].length - 2) {
+                    bylines_html += ' and';
+                }
+            }
+            bylines_html += '</p>';
+        }
+    }
+
+}
+
+function setAuthorBio(type, author) {
+    let twitter_link = `<span><a href="https://twitter.com/${author.twitter}" target="_blank">Twitter <i class="fab fa-twitter"></i></a></span>`;
+    let email_link = `<span style="padding-right: var(--xs); padding-left: var(--xxs);"><a
+        href="mailto:${author.email}" target="_blank">Email <i class="fa fa-envelope"></i></a></span>`;
+    if (author.pfp && author.bio) {
+        bios_html +=
+            `<div class="bio">
+                        <div>
+                            <img src="${author.pfp}" alt="${author.name}">
+                            <div>
+                            <p>${type} <a href="https://idsnews.com/staff/${author.name.split(' ').join('-')}" target="_blank">${author.name}</a></p>
+                            <p>${author.bio}  ${author.email ? email_link : ''}   ${author.twitter ? twitter_link : ''}</p>
+                            </div>
+                        </div>
+                    </div>`;
+    }
+}
+
+document.querySelector('#bylines').innerHTML = bylines_html;
+if(document.querySelector('.author-bios')) document.querySelector('.author-bios').innerHTML = bios_html;
+
+// pubdate
+document.querySelector('#pubdate').innerHTML = "Published " + data.pub_date;
+
+// title & slug
+document.querySelector('title').innerHTML = data.title + ' | Indiana Daily Student';
+document.querySelector('#slug').innerHTML = data.slug;
+
+// socials
+let meta_twitter = document.querySelectorAll('meta[name*="twitter"]');
+let meta_og = document.querySelectorAll('meta[property*="og"]');
+let fb = `https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fspecials.idsnews.com%2F${data.url}`;
+let twitter = `https://twitter.com/intent/tweet?url=https%3A%2F%2Fspecials.idsnews.com%2F${data.url}%2F&text=${data.headline.split(' ').join('%20')}`;
+let reddit = `https://www.reddit.com/submit?title=${data.headline.split(' ').join('%20')}&url=http%3A%2F%2Fspecials.idsnews.com%2F${data.url}`;
+
+document.querySelector('li#socials').innerHTML = `
+        <a href="${fb}" target="_blank"><i class="fab fa-facebook"></i></a>
+        <a href="${twitter}" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="${reddit}" target="_blank"><i class="fab fa-reddit"></i></a>
+        `;
