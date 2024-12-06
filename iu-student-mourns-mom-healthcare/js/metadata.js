f463b36e1e7c2a2c669770323851fcd2d22bcd77
idsnews <idsnews@users.noreply.github.com>
2024-12-06 00:19:39 -0500

    words and pictures project
    files from Matéi

---
+++ iu-student-mourns-mom-healthcare/js/metadata.js
@@ -0,0 +1,106 @@
+const data = {
+    "url": "iu-student-mourns-mom-healthcare",
+    "slug": "IU student mourns his mom",
+    "headline": "IU student mourns his mom",
+    "title": "IU student mourns his mom",
+    "pub_date": "Dec. 6, 2024",
+    "bylines": {
+        "By": {
+            "name": "Isabella Vesperini",
+            "email": "isvesp@iu.edu",
+            "twitter": "",
+            "pfp": "https://s3.amazonaws.com/snwceomedia/ids/256be6fa-97d2-4728-9909-32a56b352553.original.png",
+            "bio": "Isabella Vesperini is a junior studying journalism, political science and Italian. She currently works as a GA editor for the IDS."
+        },
+        "Photos by": {
+            "name": "Emma Walls",
+            "email": "wallemma@iu.edu",
+            "bio": "Emma Walls is a sophomore at Indiana University studying journalism. She is a photographer for the Indiana Daily Student.",
+            "pfp": "https://s3.amazonaws.com/snwceomedia/ids/8f63999e-8442-4f23-a4a9-b5b90d0be9c4.original.png"
+        },
+        "Design and development by": {
+            "name": "Ty Kalk",
+            "email": "TyKalk@iu.edu",
+            "twitter": "",
+            "pfp": "https://s3.amazonaws.com/snwceomedia/ids/51d72a51-2c9b-4abe-9709-fef8263db794.original.png",
+            "bio": "Ty Kalk is a creative advertising student attending Indiana University."
+        }
+    }
+}
+
+// bylines
+const byline_types = ["By", "Photos by", "Design and development by", "Graphics by"]
+let bylines_html = '';
+let bios_html = '';
+
+for (let type of byline_types) {
+    if (data.bylines[type]) {
+        if (data.bylines[type].length) {
+            data.bylines[type].forEach((author) => setAuthorBio(type, author));
+        } else {
+            setAuthorBio(type, data.bylines[type]);
+        }
+    }
+}
+
+for (let type of byline_types) {
+    if (data.bylines[type]) {
+
+        if (!data.bylines[type].length) {
+            bylines_html += `<p>${type} <a href="https://idsnews.com/staff/${data.bylines[type].name.split(' ').join('-')}">${data.bylines[type].name}</a></p>`;
+        } else {
+            bylines_html += '<p>' + type;
+            for (let index in data.bylines[type]) {
+                bylines_html += ` <a href="https://idsnews.com/staff/${data.bylines[type][index].name.split(' ').join('-')}">${data.bylines[type][index].name}</a>`;
+                if (index != data.bylines[type].length - 2 && index != data.bylines[type].length - 1) {
+                    bylines_html += ',';
+                } else if (index == data.bylines[type].length - 2) {
+                    bylines_html += ' and';
+                }
+            }
+            bylines_html += '</p>';
+        }
+    }
+
+}
+
+function setAuthorBio(type, author) {
+    let twitter_link = `<span><a href="https://twitter.com/${author.twitter}" target="_blank">Twitter <i class="fab fa-twitter"></i></a></span>`;
+    let email_link = `<span style="padding-right: var(--xs); padding-left: var(--xxs);"><a
+        href="mailto:${author.email}" target="_blank">Email <i class="fa fa-envelope"></i></a></span>`;
+    if (author.pfp && author.bio) {
+        bios_html +=
+            `<div class="bio">
+                        <div>
+                            <img src="${author.pfp}" alt="${author.name}">
+                            <div>
+                            <p>${type} <a href="https://idsnews.com/staff/${author.name.split(' ').join('-')}" target="_blank">${author.name}</a></p>
+                            <p>${author.bio}  ${author.email ? email_link : ''}   ${author.twitter ? twitter_link : ''}</p>
+                            </div>
+                        </div>
+                    </div>`;
+    }
+}
+
+document.querySelector('#bylines').innerHTML = bylines_html;
+document.querySelector('.author-bios').innerHTML = bios_html;
+
+// pubdate
+document.querySelector('#pubdate').innerHTML = "Published " + data.pub_date;
+
+// title & slug
+document.querySelector('title').innerHTML = data.title + ' | Indiana Daily Student';
+document.querySelector('#slug').innerHTML = data.slug;
+
+// socials
+let meta_twitter = document.querySelectorAll('meta[name*="twitter"]');
+let meta_og = document.querySelectorAll('meta[property*="og"]');
+let fb = `https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fspecials.idsnews.com%2F${data.url}`;
+let twitter = `https://twitter.com/intent/tweet?url=https%3A%2F%2Fspecials.idsnews.com%2F${data.url}%2F&text=${data.headline.split(' ').join('%20')}`;
+let reddit = `https://www.reddit.com/submit?title=${data.headline.split(' ').join('%20')}&url=http%3A%2F%2Fspecials.idsnews.com%2F${data.url}`;
+
+document.querySelector('li#socials').innerHTML = `
+        <a href="${fb}" target="_blank"><i class="fab fa-facebook"></i></a>
+        <a href="${twitter}" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="${reddit}" target="_blank"><i class="fab fa-reddit"></i></a>
+        `;
