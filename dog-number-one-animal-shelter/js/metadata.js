43d4c37d620b6678adf28997c99234940e72a931
idsnews <idsnews@users.noreply.github.com>
2024-12-13 11:55:05 -0500

    files from Matéi
    words and pictures buildout

---
+++ dog-number-one-animal-shelter/js/metadata.js
@@ -0,0 +1,104 @@
+const data = {
+    "url": "dog-number-one-animal-shelter",
+    "slug": "Dog number one",
+    "headline": "Dog number one",
+    "title": "Dog number one",
+    "pub_date": "{insert your date in metadata.json}",
+    "bylines": {
+        "By": {
+            "name": "Mia Hilkowitz",
+            "email": "mhilkowi@iu.edu",
+            "pfp": "https://s3.amazonaws.com/snwceomedia/ids/69350d57-9632-47d6-beb7-508d9a848311.original.jpg",
+            "bio": "Mia Hilkowitz is a junior studying journalism and human-centered computing. She joined the IDS in 2022 and is a managing editor of content."
+        },
+        "Photos by": {
+            "name": "Briana Pace",
+            "email": "bpace7530@gmail.com",
+            "pfp": "https://s3.amazonaws.com/snwceomedia/ids/2532cd7d-bb5f-4314-89fb-2e6c96a80ac6.original.jpg",
+            "bio": "Briana Pace has been a photographer at the IDS for two years and has also had photos published in the Wall Street Journal and BBC News."
+        },
+        "Design and development by": {
+            "name": "Ethan Roberts",
+            "email": "ethrobe@iu.edu",
+            "pfp": "https://s3.amazonaws.com/snwceomedia/ids/01de5fd8-aa8f-469f-9c3a-f536a9d99ac5.original.jpg",
+            "bio": "Ethan Roberts is a junior studying Journalism and Political Science and has been with the IDS since 2023."
+        }
+    }
+}
+
+// bylines
+const byline_types = ["By", "Photos by", "Design and development by", "Graphics by"]
+let bylines_html = '';
+let bios_html = '';
+
+for (let type of byline_types) {
+    if (data.bylines[type]) {
+        if (data.bylines[type].length) {
+            data.bylines[type].forEach((author) => setAuthorBio(type, author));
+        } else {
+            setAuthorBio(type, data.bylines[type]);
+        }
+    }
+}
+
+for (let type of byline_types) {
+    if (data.bylines[type]) {
+
+        if (!data.bylines[type].length) {
+            bylines_html += `<p>${type} <a href="https://idsnews.com/staff/${data.bylines[type].name.split(' ').join('-')}">${data.bylines[type].name}</a></p>`;
+        } else {
+            bylines_html += '<p>' + type;
+            for (let index in data.bylines[type]) {
+                bylines_html += ` <a href="https://idsnews.com/staff/${data.bylines[type][index].name.split(' ').join('-')}">${data.bylines[type][index].name}</a>`;
+                if (index != data.bylines[type].length - 2 && index != data.bylines[type].length - 1) {
+                    bylines_html += ',';
+                } else if (index == data.bylines[type].length - 2) {
+                    bylines_html += ' and';
+                }
+            }
+            bylines_html += '</p>';
+        }
+    }
+
+}
+
+function setAuthorBio(type, author) {
+    let twitter_link = `<span><a href="https://twitter.com/${author.twitter}" target="_blank">Twitter <i class="fab fa-twitter"></i></a></span>`;
+    let email_link = `<span style="padding-right: var(--xs); padding-left: var(--xxs);"><a
+        href="mailto:${author.email}" target="_blank">Email <i class="fa fa-envelope"></i></a></span>`;
+    if (author.pfp && author.bio) {
+        bios_html +=
+            `<div class="bio">
+                        <div>
+                            <img src="${author.pfp}" alt="${author.name}">
+                            <div>
+                            <p>${type} <a href="https://idsnews.com/staff/${author.name.split(' ').join('-')}" target="_blank">${author.name}</a></p>
+                            <p>${author.bio}  ${author.email ? email_link : ''}   ${author.twitter ? twitter_link : ''}</p>
+                            </div>
+                        </div>
+                    </div>`;
+    }
+}
+
+document.querySelector('#bylines').innerHTML = bylines_html;
+document.querySelector('.author-bios').innerHTML = bios_html;
+
+// pubdate
+document.querySelector('#pubdate').innerHTML = "Published " + data.pub_date;
+
+// title & slug
+document.querySelector('title').innerHTML = data.title + ' | Indiana Daily Student';
+document.querySelector('#slug').innerHTML = data.slug;
+
+// socials
+let meta_twitter = document.querySelectorAll('meta[name*="twitter"]');
+let meta_og = document.querySelectorAll('meta[property*="og"]');
+let fb = `https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fspecials.idsnews.com%2F${data.url}`;
+let twitter = `https://twitter.com/intent/tweet?url=https%3A%2F%2Fspecials.idsnews.com%2F${data.url}%2F&text=${data.headline.split(' ').join('%20')}`;
+let reddit = `https://www.reddit.com/submit?title=${data.headline.split(' ').join('%20')}&url=http%3A%2F%2Fspecials.idsnews.com%2F${data.url}`;
+
+document.querySelector('li#socials').innerHTML = `
+        <a href="${fb}" target="_blank"><i class="fab fa-facebook"></i></a>
+        <a href="${twitter}" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="${reddit}" target="_blank"><i class="fab fa-reddit"></i></a>
+        `;
