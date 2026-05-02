8b05de247c16f3a29670fae7989b56d876844980
Matei Cloteaux <matei.cloteaux@gmail.com>
2026-05-02 11:52:09 -0400

    dc

---
+++ data-center-morgan-county-indiana-billion-dollar/js/metadata.js
@@ -0,0 +1,106 @@
+const data = {
+    "url": "data-center-morgan-county-indiana-billion-dollar",
+    "slug": "Data Center",
+    "headline": "The billion-dollar neighbor coming to Morgan County",
+    "title": "Data Center",
+    "pub_date": "May 2, 2026",
+    "bylines": {
+        "Story and photos by": {
+            "name": "Ella Curlin",
+            "email": "elcurlin@iu.edu",
+            // "twitter": "catcharron",
+            "pfp": "https://s3.amazonaws.com/snwceomedia/ids/f606ecc1-cbcb-4659-9bf0-28e820e45006.original.jpg",
+            "bio": "Ella Curlin has worked for the IDS since 2024 and is a news editor."
+        },
+		"Photos by": {
+			"name": "Emerson Elledge",
+			"email": "eelledge@iu.edu",
+			"pfp": "https://s3.amazonaws.com/snwceomedia/ids/db1f07e5-bd05-46b2-8082-4d568726b42c.original.jpg",
+			"bio": "Emerson Elledge has worked for the IDS since 2024 as a writer and photographer."
+		},
+        "Design and development by": {
+            "name": "Calvin Josenhans",
+            "email": "cjosenha@iu.edu",
+            "twitter": "CalvinJosenhans",
+            "pfp": "https://s3.amazonaws.com/snwceomedia/ids/13acda73-a88b-4fb5-bb21-8a4342e8a14c.original.jpg",
+            "bio": "Calvin has worked at the IDS since 2024 on the digital desk."
+        },
+    }
+}
+
+// bylines
+const byline_types = ["Story and photos by","Design and development by", "Photos by"]
+let bylines_html = '';
+let bios_html = '';
+
+for (let type of byline_types) {
+    if (data.bylines[type]) {
+        if (data.bylines[type].length) {
+            data.bylines[type].forEach((author) => setAuthorBio(type, author));
+        } else {
+            setAuthorBio(type, data.bylines[type]);
+        }
+    }
+}
+
+for (let type of byline_types) {
+    if (data.bylines[type]) {
+
+        if (!data.bylines[type].length) {
+            bylines_html += `<p>${type} <a href="https://idsnews.com/staff/${data.bylines[type].name.split(' ').join('-')}">${data.bylines[type].name}</a></p>`;
+        } else {
+            bylines_html += '<p>' + type;
+            for (let index in data.bylines[type]) {
+                bylines_html += ` <a href="https://idsnews.com/staff/${data.bylines[type][index].name.split(' ').join('-')}">${data.bylines[type][index].name}</a>`;
+                if (index != data.bylines[type].length - 2 && index != data.bylines[type].length - 1) {
+                    bylines_html += ',';
+                } else if (index == data.bylines[type].length - 2) {
+                    bylines_html += ' and';
+                }
+            }
+            bylines_html += '</p>';
+        }
+    }
+
+}
+
+function setAuthorBio(type, author) {
+    let twitter_link = `<span><a href="https://twitter.com/${author.twitter}" target="_blank">Twitter <i class="fab fa-twitter"></i></a></span>`;
+    let email_link = `<span style="padding-right: var(--xs); padding-left: var(--xxs);"><a
+        href="mailto:${author.email}" target="_blank">Email <i class="fa fa-envelope"></i></a></span>`;
+    if (author.pfp && author.bio) {
+        bios_html +=
+            `<div class="bio">
+                        <div>
+                            <img src="${author.pfp}" alt="${author.name}">
+                            <div>
+                            <p>${type} <a href="https://idsnews.com/staff/${author.name.split(' ').join('-')}" target="_blank">${author.name}</a></p>
+                            <p>${author.bio}  ${author.email ? email_link : ''}   ${author.twitter ? twitter_link : ''}</p>
+                            </div>
+                        </div>
+                    </div>`;
+    }
+}
+
+document.querySelector('#bylines').innerHTML = bylines_html;
+document.querySelector('.author-bios').innerHTML = bios_html;
+
+// pubdate
+document.querySelector('#pubdate').innerHTML = "Published " + data.pub_date;
+
+// title & slug
+document.querySelector('title').innerHTML = data.title + ' | Indiana Daily Student';
+document.querySelector('#slug').innerHTML = data.slug;
+
+// socials
+let meta_twitter = document.querySelectorAll('meta[name*="twitter"]');
+let meta_og = document.querySelectorAll('meta[property*="og"]');
+let fb = `https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fspecials.idsnews.com%2F${data.url}`;
+let twitter = `https://twitter.com/intent/tweet?url=https%3A%2F%2Fspecials.idsnews.com%2F${data.url}%2F&text=${data.headline.split(' ').join('%20')}`;
+let reddit = `https://www.reddit.com/submit?title=${data.headline.split(' ').join('%20')}&url=http%3A%2F%2Fspecials.idsnews.com%2F${data.url}`;
+
+document.querySelector('li#socials').innerHTML = `
+        <a href="${fb}" target="_blank"><i class="fab fa-facebook"></i></a>
+        <a href="${twitter}" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="${reddit}" target="_blank"><i class="fab fa-reddit"></i></a>
+        `;
